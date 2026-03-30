a2196c2c36429ffd677724f1cb8e2ea534c41567
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-30 17:35:31 +0600

    pricing section work on going

---
+++ src/Pricing.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Pricing = () => {
+  return (
+    <div className="text-center my-16">
+      <h2 className="text-3xl font-bold">Pricing</h2>
+      <p className="text-gray-500">
+        Simple & affordable pricing plans
+      </p>
+    </div>
+  );
+};
+
+export default Pricing;
