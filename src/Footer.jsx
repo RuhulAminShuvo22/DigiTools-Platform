418e49770332fb58862aaadc0a57b074fd5c1178
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-30 17:36:21 +0600

    footer section added

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer p-10 bg-base-200 text-base-content mt-10">
+      <p>© 2026 DigiTools. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
