90f1c67800a927fa151e713c0dc5ebc495b9981c
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-03-30 17:37:34 +0600

    app.jsx erlated work in ongoing

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Banner from "./components/Banner";
+import Stats from "./components/Stats";
+import ToggleSection from "./components/ToggleSection";
+import Steps from "./components/Steps";
+import Pricing from "./components/Pricing";
+import Footer from "./components/Footer";
+import productsData from "./data/data.json";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  const [cart, setCart] = useState([]);
+  const [activeTab, setActiveTab] = useState("products");
+
+  // Add to cart
+  const handleAddToCart = (product) => {
+    const exists = cart.find((item) => item.id === product.id);
+
+    if (exists) {
+      toast.warning("Already added!");
+      return;
+    }
+
+    setCart([...cart, product]);
+    toast.success("Added to cart!");
+  };
+
+  // Remove item
+  const handleRemove = (id) => {
+    const updated = cart.filter((item) => item.id !== id);
+    setCart(updated);
+    toast.error("Removed from cart!");
+  };
+
+  // Checkout
+  const handleCheckout = () => {
+    if (cart.length === 0) {
+      toast.warning("Cart is empty!");
+      return;
+    }
+
+    setCart([]);
+    toast.info("Checkout successful!");
+  };
+
+  return (
+    <>
+      <Navbar cartCount={cart.length} />
+
+      <Banner />
+      <Stats />
+
+      <ToggleSection
+        products={productsData}
+        cart={cart}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+        handleAddToCart={handleAddToCart}
+        handleRemove={handleRemove}
+        handleCheckout={handleCheckout}
+      />
+
+      <Steps />
+      <Pricing />
+      <Footer />
+
+      <ToastContainer position="top-right" />
+    </>
+  );
+}
+
+export default App;
